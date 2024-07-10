9f9b89702401e967446b272ca4bc6ca9a1de2ad3
xyugen <alex.arias.jorym@gmail.com>
2024-07-11 04:35:10 +0800

    feat: make anime info page more aesthetic

--- src/app/anime/[id]/page.tsx
+++ src/app/anime/[id]/page.tsx
@@ -1,17 +1,14 @@
-import Header from '@/components/header'
 import { Button } from '@/components/ui/button'
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@/components/ui/table'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { ANIME, IAnimeInfo, ITitle, META } from '@consumet/extensions'
+import { IAnimeInfo, META } from '@consumet/extensions'
 import { Star } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const Page = async ({ params }: { params: { id: string }}) => {
-  const gogoanime = new ANIME.Gogoanime();
   const anilist = new META.Anilist();
-  const anime = await gogoanime.fetchAnimeInfo(params.id);
   const meta = await anilist.search(params.id);
   const animeInfo: IAnimeInfo = await anilist.fetchAnimeInfo(meta.results[0].id);
   
@@ -22,13 +19,13 @@ const Page = async ({ params }: { params: { id: string }}) => {
   return (
     <main className='p-2 md:p-12 lg:p-24'>
       <div className='flex flex-col gap-14 md:gap-20'>
-        <div className='flex gap-2 md:gap-8'>
+        <div className='flex gap-2 md:gap-8 flex-wrap'>
           <Image
             src={animeInfo.image!}
             alt={animeInfo.title.english}
             width={200}
             height={200}
-            className='rounded-md h-40 md:h-fit object-cover'
+            className='rounded-md h-fit object-cover'
             draggable='false'
           />
 
@@ -44,7 +41,9 @@ const Page = async ({ params }: { params: { id: string }}) => {
               <span className='text-sm'>{animeInfo.rating || 'N/A'}</span>
             </div>
             <div className='flex gap-2 flex-wrap'>
-              <Button variant='default' className='w-full sm:w-fit'>Start Watching</Button>
+              <Button variant={'default'} className='w-full sm:w-fit' asChild>
+                <Link href={`/anime/${params.id}/episode/1`}>Start Watching</Link>
+              </Button>
               <Button variant='outline' className='w-full sm:w-fit'>Add to My List</Button>
             </div>
           </div>
@@ -56,11 +55,11 @@ const Page = async ({ params }: { params: { id: string }}) => {
             <TabsTrigger value='episodes'>Episodes</TabsTrigger>
           </TabsList>
           <TabsContent value='overview'>
-            <div className='flex gap-4'>
+            <div className='flex gap-4 flex-wrap-reverse md:flex-nowrap'>
               {/* Details */}
               <div className='flex flex-col gap-4'>
                 <h2 className='text-xl font-bold'>Details</h2>
-                <Table className='w-64'>
+                <Table className='w-full md:w-64'>
                   <TableBody>
                     <TableRow>
                       <TableHead>Type</TableHead>
@@ -123,19 +122,19 @@ const Page = async ({ params }: { params: { id: string }}) => {
           </TabsContent>
           <TabsContent value='episodes'>
             <h2 className='text-xl font-bold'>Episodes</h2>
-            <div className='flex flex-wrap gap-7'>
+            <div className='flex flex-wrap gap-7 '>
               {animeInfo.episodes?.map((episode, index) => (
-                <Link href={`/anime/${params.id}/episode/${index + 1}`} key={index}>
+                <Link href={`/anime/${params.id}/episode/${index + 1}`} key={index} className='max-w-full w-full md:max-w-[160px] p-2 hover:bg-primary/40 rounded-sm'>
                   <Image
                     src={episode.image!}
                     alt={episode.title!}
-                    width={150}
-                    height={150}
+                    width={400}
+                    height={200}
                     className='rounded-md h-40 md:h-fit object-cover'
                     draggable='false'
                   />
-                  <h3 className='text-base font-medium'>{episode.title}</h3>
-                  <p className='text-foreground/70'>{episode.releaseDate}</p>
+                  <h3 className='text-base font-medium text-wrap'>Episode {index + 1}</h3>
+                  <p className='text-sm text-foreground/70'>{episode.title}</p>
                 </Link>
               ))}
             </div>
